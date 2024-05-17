75ec043f82918376b1e53c54e1b2fba81a0b9c0b
Adam Fine <afine2@uchicago.edu>
2024-05-16 19:13:03 -0500

    1

--- figures/gb_dataset/gb_figures.py
+++ figures/gb_dataset/gb_figures.py
@@ -18,6 +18,11 @@ classification_types = ["add", "dom", "rec", "het"]
 def combine_pos(row):
     return f"{int(row['Left pos.']):,d}-{int(row['Right pos.']):,d}"
 
+def special_format(flt):
+    if flt > 1:
+        return f"{flt:.2f}"
+    return f"{flt:.3f}"
+
 plt.rcParams.update({'font.size': 9,
                      'text.usetex': False,
                      'font.family': 'serif',
@@ -158,11 +163,13 @@ for c_type in classification_types:
                              sw_idxmax, sw_argpmax, sw_rsidmax, sw_ref, sw_alt, sw_raw_snps, sw_snps, sw_chridxmaxs, sw_genes]).T
         brown_windows = pd.DataFrame(sw_array, columns=["Sel. type", "Chr.", "Left pos.", "Right pos.", "Raw", "Post", r"$-\log_{10}p_{min}", r"$\hat{s}(p_{min})", r"ll at $s(p_{min})$", "SNP index of max.", "Chr pos of max.", "Lead SNP", "Ref.", "Alt.", "Raw_SNP_list", "Post_SNP_list", "SNP. index of max (on chr).", "Gene(s)"])
         brown_windows["Genomic region (hg19)"] = brown_windows.apply(combine_pos, axis=1)
+        brown_windows[r"$-\log_{10}p_{min}"] = brown_windows[r"$-\log_{10}p_{min}"].astype(float)
+        brown_windows[r"$\hat{s}(p_{min})"] = brown_windows[r"$\hat{s}(p_{min})"].astype(float)
         brown_windows = brown_windows[["Sel. type", "Chr.", "Genomic region (hg19)", "Gene(s)", "Lead SNP", "Ref.", "Alt.",
              "Raw", "Post", r"$-\log_{10}p_{min}", r"$\hat{s}(p_{min})",
              r"ll at $s(p_{min})$", "SNP index of max.", "Chr pos of max.",
              "Raw_SNP_list", "Post_SNP_list", "SNP. index of max (on chr)."]]
-        brown_windows.to_latex(f"{output_dir}/{genodata_type}_{c_type}_sig_windows.tex", float_format="%.2f",
+        brown_windows.to_latex(f"{output_dir}/{genodata_type}_{c_type}_sig_windows.tex", float_format=special_format,
                                columns=["Chr.", "Genomic region (hg19)", "Gene(s)", "Lead SNP", "Ref.", "Alt.",
                                          "Raw", "Post", r"$-\log_{10}p_{min}", r"$\hat{s}(p_{min})"],
                                index=False, column_format="cccccccccc")
@@ -189,9 +196,10 @@ for c_type in classification_types:
     plt.close(fig)
 
 #qq plots
-fig, axs = plt.subplots(1,1,figsize=(3.1, 3.1),layout="constrained", dpi=1500)
-axins = axs.inset_axes([.67, .11, .28, .28])
-logps = [cdata['all_p'][f'{ctype}_p'] for ctype in classification_types]
-labels = convert_from_abbrevs(classification_types, shorthet=True)
-plot_qq(axs, axins, logps, labels, legend_loc="upper right", thin=True)
-fig.savefig(f"{output_dir}/{genodata_type}_all_qqs.png", format="png", bbox_inches="tight")
+if len(classification_types) > 1:
+    fig, axs = plt.subplots(1,1,figsize=(3.1, 3.1),layout="constrained", dpi=1500)
+    axins = axs.inset_axes([.67, .11, .28, .28])
+    logps = [cdata['all_p'][f'{ctype}_p'] for ctype in classification_types]
+    labels = convert_from_abbrevs(classification_types, shorthet=True)
+    plot_qq(axs, axins, logps, labels, legend_loc="upper right", thin=True)
+    fig.savefig(f"{output_dir}/{genodata_type}_all_qqs.png", format="png", bbox_inches="tight")
